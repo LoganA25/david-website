b5e877ff5b2b83e23485d453344c2b7d46237a21
Logan Allen <logandallen25@gmail.com>
2025-01-16 00:40:55 -0600

    Fixed button issue

--- src/components/Youtube.tsx
+++ src/components/Youtube.tsx
@@ -3,12 +3,10 @@ import { FaYoutube } from 'react-icons/fa'
 export function Youtube() {
     return (
         <>
-            <button className="inline-flex rounded-full px-4 space-x-4 text-lg font-semibold transition bg-black justify-center items-center hover:text-black hover:bg-white">
-                <a href="https://www.youtube.com/@cytechflow6273" className="text-red-600 text-8xl">
-                    <FaYoutube />
-                </a>
+            <a href="https://www.youtube.com/@cytechflow6273" className="inline-flex rounded-full px-4 space-x-4 text-lg font-semibold transition bg-black justify-center items-center hover:text-black hover:bg-white">
+                <FaYoutube className="text-red-600 text-8xl"/>
                 <p>Checkout my channel!</p>
-            </button>
+            </a>
         </>
     )
 }
